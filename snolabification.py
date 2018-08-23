00e51b31741ba162e210f5d1d9c776969236a914
Étienne <27770178+bourdeet@users.noreply.github.com>
2018-08-23 16:14:37 +0200

    snolabification of folders

---
+++ snolabification.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+
+##################################################
+#              wavedef_jobs.py
+#
+# runs process_to_wavedeform on a given folder
+##################################################
+
+
+
+if __name__=='__main__':
+
+
+
+    # Get the name of the folder
+    #======================================================
+    
+    import argparse
+    
+    parser = argparse.ArgumentParser("snolabify an entire folder!")
+    parser.add_argument("--folder",
+                        required = True,
+                        help="Folder containing DOMlaunched i3 files")
+    
+    parser.add_argument('--dom',
+                        help='dom for which you want a snolab-style pulse series',
+                        default='20-11')
+    
+    args = parser.parse_args()
+
+
+    # list files in the folder:
+    #=======================================================
+
+    import glob
+    files_to_process = sorted(glob.glob(args.folder+"/*.i3.*"))
+
+    # Produce an output directory for the dom you are analyzing
+    #=======================================================
+
+    new_directory = folder+"/"+args.dom+"/"
+
+
+    # Loop over file and run process_wavedeform
+    #=======================================================
+
+    import snolabify_vuvuzela as s
+
+    for f in files_to_process:
+
+        if 'wavedeformed' not in f:
+
+            # Retrieve the file number and define the output file name
+
+            core_file = f.split(".i3")[0]
+            file_number  = float(core_file.split('_')[-1])
+            new_f = "snolabified_"+f.split("/")[-1].split(".i3")[0]+".p"
+
+            print new_f
+            
+            #sno.snolabify(f,new_directory+new_f,pseries,target_dom)
+    
+
+        
