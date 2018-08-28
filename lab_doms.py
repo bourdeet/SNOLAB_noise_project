fa702b01541c691eecb4f77d4f31a28f15fdd955
Étienne <27770178+bourdeet@users.noreply.github.com>
2018-08-28 12:02:07 +0200

    added lab_doms.py

---
+++ lab_doms.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+
+# produce  dictionary for each DOM tested. mark up the number
+# of the good runs that shall represent a DOM's performance
+# at a given temperature setting.
+#
+# That info is taken out of results_FINAL.ods
+#------------------------------------------------------------------
+
+Aludsparken = {'name':'Aluddsparken',
+               'T10':184,
+               'T20':180,
+               'T30':177,
+               'T40':172,
+               'scale':150,
+               'spe':5.13,
+               'qpair':20
+               }
+
+Skogshare = {'name':'Skogshare',
+             'T10':136,
+             'T20':129,
+             'T30':124,
+             'T40':143,
+             'scale':150,
+             'spe':2.1,
+             'qpair':14
+             }
+
+Ingladsil = {'name':'Ingladsil',
+             'T10':108,
+             'T20':127,
+             'T30':126, #,116]
+             'T40':116,
+             'scale':50,
+             'spe':2.1,
+             'qpair':7.
+             }
+
+Antarctica = {'name':'Antarctica',
+              'T10':134,
+              'T20':128,
+              'T30':123,
+              'T40':141,
+              'scale': 100,
+              'spe':2.12,
+              'qpair':7.
+               }
+
+doms_to_plot = [Aludsparken, Skogshare, Ingladsil, Antarctica]
+
